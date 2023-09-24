9a10d7da3fc43f2ce5744b4045914aaa96a6e454
Bakyt <bakyt.gaipov.kk@gmail.com>
2023-09-24 22:41:20 +0600

    create signup routes

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,19 @@
-const signup = (req, res) => {
-    console.log(req.body);
+import User from '../models/userModel.js';
+import bcrypt from 'bcryptjs';
+
+const signup = async (req, res) => {
+    const {username, email, password} = req.body;
+    const hashedPassword = await bcrypt.hashSync(password, 10);
+    const user = new User({username, email, password: hashedPassword});
+
+    try {
+        await user.save();
+        res.status(200).json({
+            message: "Signup successfully"
+        });
+    } catch (error) {
+        res.status(500).json(error.message);
+    }
 };
 
 export {
